4934d952d22f657759b4633e643d7d2d7f82ffd1
Baiysh22 <aitikeev9@gmail.com>
2022-07-07 16:51:58 +0600

    first commit

---
+++ src/components/Buy/Buy.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Buy = () => {
+  return (
+    <div className='section'>
+      <div className="cart">
+      <img className='cart' src="https://habrastorage.org/web/313/c0e/dc4/313c0edc4eb8432fa68a3a8ef479d5e1.gif" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Buy;
